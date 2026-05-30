457af0abb3c6a969b9b7185b518db89af7774b29
Elisabeth-Matulian <elisabethmatulian@gmail.com>
2026-05-30 16:03:30 +0100

    add functions: createBookmark, sortBookmarks; add: cache

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
-import { setData } from "./storage"
+import { getData, setData } from "./storage.js"
+
+// === DATA ===
+let cache = {};
 
 // === DOM ELEMENTS ===
 const bookmarkForm = document.getElementById("bookmarkForm");
@@ -12,7 +15,20 @@ const userSelect = document.getElementById("userSelect");
 // === ENTRY POINT === 
 
 // === SORTING BOOKMARKS ===
+function sortBookmarks(userId) {
+    let bookmarks = getData(userId);
+    return bookmarks.sort((a, b) => b.time - a.time);
+}
 
 // === RENDERING BOOKMARKS ===
 
 // === A BOOKMARK BUILDER ===
+function createBookmark(title, url, description) {
+    return {
+        title: title,
+        url: url,
+        description: description,
+        likes: 0,
+        time: new Date().toISOString()
+    }
+}
