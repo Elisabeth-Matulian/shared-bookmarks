962d46917d1c43eaf16cdcd8f5882c594a2a8772
Elisabeth-Matulian <elisabethmatulian@gmail.com>
2026-05-30 13:18:14 +0100

    add: DOM element in JS; bookmark title for HTML

---
+++ script.js
@@ -0,0 +1,18 @@
+
+import { setData } from "./storage"
+
+// === DOM ELEMENTS ===
+const bookmarkForm = document.getElementById("bookmarkForm");
+const bookmarkTitle = document.getElementById("bookmarkTitle");
+const bookmarkURL = document.getElementById("bookmarkURL");
+const bookmarkDescription = document.getElementById("bookmarkDescription");
+const bookmarksBox = document.getElementById("bookmarksBox");
+const userSelect = document.getElementById("userSelect");
+
+// === ENTRY POINT === 
+
+// === SORTING BOOKMARKS ===
+
+// === RENDERING BOOKMARKS ===
+
+// === A BOOKMARK BUILDER ===
